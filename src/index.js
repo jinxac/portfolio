ac3ed684634683c5ff4104d3ca55de9ed922f920
Harkirat <harkirat@wealthy.in>
2017-04-04 21:32:26 +0530

    adding base routes + linter + babelrc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('heya')
+import React from "react";
+import PortfolioRoutes from "./Routes";
+import {render} from "react-dom";
+
+render(
+  <PortfolioRoutes />,
+  document.getElementById("app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
-var path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-var BUILD_DIR = path.resolve(__dirname, 'build')
-var APP_DIR = path.resolve(__dirname, 'src')
+var BUILD_DIR = path.resolve(__dirname, "build");
+var APP_DIR = path.resolve(__dirname, "src");
 
 module.exports = {
   entry: APP_DIR,
   output: {
     path: BUILD_DIR,
-    filename: '[name]-[hash].js'
+    filename: "[name]-[hash].js"
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
         include: APP_DIR,
-        loader: 'babel-loader'
+        loader: "babel-loader",
+        exclude:/node_modules/,
+        query:{
+          presets: ["es2015","react","stage-0"]
+        }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.ejs'
+      template: "index.ejs"
     })
   ]
-}
+};
