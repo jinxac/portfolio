0c3a29f9a960bd749fd38a5cd336eaac0df1ad32
Harkirat <harkirat@wealthy.in>
2017-04-04 22:59:40 +0530

    webpack 2 config with rules and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,55 @@
-var path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
-var BUILD_DIR = path.resolve(__dirname, "build");
-var APP_DIR = path.resolve(__dirname, "src");
+
+const BUILD_DIR = path.resolve(__dirname, "build");
+const APP_DIR = path.resolve(__dirname, "src");
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "index.ejs"
+  }),
+  new ExtractTextPlugin("styles.css"),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      postcss: [
+        autoprefixer({
+          browsers: [
+            "last 3 version",
+            "ie >= 10"
+          ]
+        })
+      ]
+    }
+  })
+];
+
+const rules = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: [{
+      loader: "babel-loader",
+      options: {
+        presets: ["es2015","react","stage-0"]
+      }
+    }]
+  },
+  {
+    test: /\.(png|gif|jpg|svg)$/,
+    use: "url-loader?limit=20480&name=assets/[name]-[hash].[ext]"
+  },
+  {
+    test: /\.css$/,
+    loader: ExtractTextPlugin.extract({
+      fallbackLoader: "style-loader",
+      loader: "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader"
+    })
+  }
+];
 
 module.exports = {
   entry: APP_DIR,
@@ -12,26 +58,7 @@ module.exports = {
     filename: "[name]-[hash].js"
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        include: APP_DIR,
-        loader: "babel-loader",
-        exclude:/node_modules/,
-        query:{
-          presets: ["es2015","react","stage-0"]
-        }
-      },
-      {
-        test: /\.css/,
-        loader: ExtractTextPlugin.extract("css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]")
-      }
-    ]
+    rules
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.ejs"
-    }),
-    new ExtractTextPlugin("styles.css")
-  ]
+  plugins
 };
